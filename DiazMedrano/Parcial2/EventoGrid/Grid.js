2b62c6711ebe90a33449d7112b442cf29c7778c4
DARKINGER <alex10200110@gmail.com>
2022-04-05 14:17:21 -0600

    UsoDeJQuerry

--- DiazMedrano/Parcial2/EventoGrid/Grid.js
+++ DiazMedrano/Parcial2/EventoGrid/Grid.js
@@ -1,3 +1,15 @@
-function name() {
-    
-}
+  //      window.onload = function(){
+  //      document.getElementById('Content').addEventListener("click",function(evento){
+  //          document.getElementById(evento.target.id).classList.toggle('Seccion-Selectionada');
+  //          });
+  //      }
+
+        //zeal para documentaciones
+
+
+$(document).ready(function(){
+    $('#Content').click(function (e){
+        $(e.target).toggleClass('Seccion-Selectionada')
+    })
+})
+
